11e523cedbab82d0e67d83f7faf702266a2427fd
Richard Lavoura <richardlavoura@macbookpro.home>
2025-09-17 17:21:29 +0200

    feat: add extract_account_keys helper function

--- src/infra/solana_rpc.rs
+++ src/infra/solana_rpc.rs
@@ -1,6 +1,9 @@
+use solana_transaction_status_client_types::EncodedConfirmedTransactionWithStatusMeta;
 use crate::error::{self, Result};
 use solana_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::{account::Account, pubkey::Pubkey};
+use solana_transaction_status_client_types::UiMessage;
+use solana_transaction_status_client_types::EncodedTransaction;
 
 pub async fn fetch_account(rpc: &RpcClient, address: Pubkey) -> Result<Account> {
     let mut accounts = rpc.get_multiple_accounts(&[address]).await?;
@@ -8,4 +11,20 @@ pub async fn fetch_account(rpc: &RpcClient, address: Pubkey) -> Result<Account> 
         .pop()
         .flatten()
         .ok_or(error::Error::TokenNotFound(address))
+}
+
+pub fn extract_account_keys(
+    tx: &EncodedConfirmedTransactionWithStatusMeta,
+) -> Option<Vec<String>> {
+    let enc = &tx.transaction.transaction; // Option<EncodedTransaction>
+
+    match enc {
+        EncodedTransaction::Json(ui_tx) => match &ui_tx.message {
+            UiMessage::Raw(raw) => Some(raw.account_keys.clone()),
+            UiMessage::Parsed(parsed) => {
+                Some(parsed.account_keys.iter().map(|k| k.pubkey.clone()).collect())
+            }
+        },
+        _ => None, // Legacy/Binary
+    }
 }
