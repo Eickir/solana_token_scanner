81832274a2ba17551a077c1c87abc0519ae3db59
Richard Lavoura <richardlavoura@macbookpro.home>
2025-09-19 17:40:14 +0200

    feat: add EventDecoder trait that will decode log. Will be implemented per platform

---
+++ src/domain/event_decoder/event_decoder.rs
@@ -0,0 +1,19 @@
+use crate::domain::event_decoder::decode_error::Result;
+use crate::platforms::platforms::Platform;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum EventKind {
+    Create,
+    Trade,
+    Unknown, 
+}
+
+pub trait EventDecoder {
+    type Create;
+    type Trade;
+
+    fn platform(&self) -> Platform;
+    fn classify(&self, payload: &[u8]) -> Option<EventKind>;
+    fn decode_create(&self, payload: &[u8]) -> Result<Self::Create>;
+    fn decode_trade(&self, payload: &[u8]) -> Result<Self::Trade>;
+}
